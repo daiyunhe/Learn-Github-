ee396ee1ac0d7512c558dbc9f44712cb15cf6181
Cristiano <34684247+daiyunhe@users.noreply.github.com>
2020-02-08 14:49:29 +0800

    Update this file
    add another predict

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,6 +1,8 @@
 form sklearn import tree
 y = [0, 0, 2] # 花类别
 clf = tree.DecisionTreeClassifier(criterion = "entropy")
-clf = clf.fit(X, y)
+
 # After being fitted, the model can then be used to predict the class of samples:
 print(clf.predict([[2., 2., 3., 4.],[2., 2., 3., 4.]]))
+print(clf.predict([2,2,2,2])
+      
